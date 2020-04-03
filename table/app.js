b7e3c100a4b01cfd8fe751494b03b928a73b2808
svetik <ternasvet@gmail.com>
2020-04-03 21:35:11 +0300

    реализация localStorage

--- table/app.js
+++ table/app.js
@@ -1,35 +1,34 @@
-// let arr = [{
-//     name: 'Don',
-//     surname: 'Smith',
-//     age: 25,
-//     country: 'Ukraine',
-//     male: 'man'
-// }, {
-//     name: 'Mikle',
-//     surname: 'Miller',
-//     age: '',
-//     country: 'Germany'
-// }, {
-//     name: 'Michael',
-//     surname: 'Kors',
-//     age: 60,
-//     country: 'USA',
-//     male: ''
-// }, {
-//     name: 'Denis',
-//     surname: '',
-//     age: 32,
-//     country: '',
-//     male: 'man'
-// }, {
-//     name: 'Shani',
-//     surname: 'Mizrachi',
-//     age: 34,
-//     country: 'Israel',
-//     male: 'women'
-// }
-// ];
-
+let arr = [{
+    name: 'Don',
+    surname: 'Smith',
+    age: 25,
+    country: 'Ukraine',
+    male: 'man'
+}, {
+    name: 'Mikle',
+    surname: 'Miller',
+    age: '',
+    country: 'Germany'
+}, {
+    name: 'Michael',
+    surname: 'Kors',
+    age: 60,
+    country: 'USA',
+    male: ''
+}, {
+    name: 'Denis',
+    surname: '',
+    age: 32,
+    country: '',
+    male: 'man'
+}, {
+    name: 'Shani',
+    surname: 'Mizrachi',
+    age: 34,
+    country: 'Israel',
+    male: 'women'
+}
+];
 // Модальное окно
 let modal = document.querySelector('.modal-background');
 let closeButton = document.querySelector('.close');
@@ -52,9 +51,6 @@ table.classList.add('table-dark', 'table-bordered', 'table');
 wrapper.append(modalButton);
 wrapper.append(table);
 document.body.append(wrapper);
-
-// Пагинация
-let pagination = document.querySelector('.pagin');
 
 function tableObj(arr) {
     let modalData = {};
@@ -88,12 +84,11 @@ function tableObj(arr) {
         modalData = {};
         closeModal();
         outText.innerHTML = '';
+        setDataForTable(arr);
     });
 }
 
 function render(arr) {
-    let countPage = 5;
-    let page = 1;
     table.innerHTML = '';
     let tr2 = document.createElement('tr');
     let columnNames = getColumns(arr);
@@ -104,28 +99,24 @@ function render(arr) {
             tr2.append(th);
         });
         table.prepend(tr2);
-        paginationPage(arr);
     }
     if (Array.isArray(arr)) {
-        arr.slice(countPage * (page - 1), countPage * page).forEach((item) => {
-            arr.forEach((item) => {
-                let tr = document.createElement('tr');
+        arr.forEach((item) => {
+            let tr = document.createElement('tr');
 
-                columnNames.forEach((key) => {
-                    let td = document.createElement('td');
-                    td.innerText = item[key] || '';
-                    tr.append(td);
-                });
-                table.append(tr);
+            columnNames.forEach((key) => {
+                let td = document.createElement('td');
+                td.innerText = item[key] || '';
+                tr.append(td);
             });
-            paginationPage();
+            table.append(tr);
         });
-
     }
-
     document.body.append(table);
 
 }
+
+render(getDataForTable(arr));
 
 function getColumns(arr) {
     let columns = [];
@@ -150,51 +141,17 @@ function closeModal() {
     modal.style.display = 'none';
 }
 
+tableObj(arr);
 
-fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
-    .then(function (response) {
-            if (response.status !== 200) {
-                console.log('Looks like there was a problem. Status Code: ' +
-                    response.status);
-                return;
-            }
-            response.json().then(function (data) {
-                tableObj(data);
-                render(data);
-            });
-        }
-    )
-    .catch(function (err) {
-        console.log('Fetch Error :-S', err);
-    });
-
-//
-function paginationPage(arr) {
-    let pagerWrapper = document.createElement('div');
-    pagerWrapper.classList.add('pager-wrapper');
-
-    let countPage = 5;
-    //let page = 1;
-    let pageAll = Math.ceil(arr.length / countPage); //кол-во страниц
-    let pagerElems = [];
-    for (let i = 0; i < pageAll; i++) {
-        let pagerItem = document.createElement('span');
-        pagerItem.innerHTML = i + 1;
-        pagerItem.classList.add('pager-item');
-        console.log(pagerItem.innerHTML);
-        pagerItem.addEventListener('click', function () {
-            let page = Number(pagerItem.innerHTML);
-            console.log(page);
-            this.page = page;
-            render();
-        });
-
-        pagerElems.push(pagerItem);
-        pagerWrapper.append(pagerItem);
+function getDataForTable(defaultData){
+    if (localStorage.getItem('myObj')) {
+        console.log(localStorage.getItem('myObj'));
+        return JSON.parse(localStorage.getItem('myObj'));
     }
 
-    wrapper.append(pagerWrapper);
+    return defaultData;
 }
 
-
-//paginationPage(arr);
+function setDataForTable(data) {
+    localStorage.setItem('myObj', JSON.stringify(data));
+}
